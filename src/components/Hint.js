8c63bd8515ac8ed856ff79df46a3d8f1b0df092b
Benson Davis <bensondavis61@gmail.com>
2022-10-31 23:55:27 +0530

    hint animations added

--- src/components/Hint.js
+++ src/components/Hint.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Chip from "@mui/material/Chip";
 import { Stack } from "@mui/system";
-import { Typography } from "@mui/material";
+import { Box, Collapse, Typography } from "@mui/material";
 
 export default function Hint({ word, open }) {
   const [hints, setHints] = useState([]);
@@ -24,20 +24,20 @@ export default function Hint({ word, open }) {
   }, [url]);
 
   return (
-    <>
-      {open ? (
-        <Stack
-          direction={"row"}
-          spacing={1}
-          alignItems="center"
-          justifyContent={"center"}
-        >
-          <Typography>Related words :</Typography>
-          {hints.map((key, index) => (
-            <Chip label={key} color="primary" key={index} />
-          ))}
-        </Stack>
-      ) : null}
-    </>
+      <Box sx={{ m: 1 }}>
+        <Collapse in={open}>
+          <Stack
+            direction={"row"}
+            spacing={1}
+            alignItems="center"
+            justifyContent={"center"}
+          >
+            <Typography>Related words :</Typography>
+            {hints.map((key, index) => (
+              <Chip label={key} color="primary" key={index} sx={{ mr: 1 }} />
+            ))}
+          </Stack>
+        </Collapse>
+      </Box>
   );
 }
